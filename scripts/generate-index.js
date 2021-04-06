24cc26f5a5c3e338ef718cdc68a6cb39d2c56f3d
Magnus Holm <judofyr@gmail.com>
2021-04-06 13:27:25 +0200

    Introduce versioning

---
+++ scripts/generate-index.js
@@ -0,0 +1,82 @@
+const formatDate = require('date-fns/format')
+const fs = require('fs')
+
+function render(versions) {
+  const rows = versions.map(({slug, name, date, variant}) => {
+    return `<tr>
+      <td>${variant ? `<em>${variant}</em>` : ''}</td>
+      <td><a href="${slug}">${name}</a></td>
+      <td>${formatDate(date, 'MMM d, yyyy')}</td>
+    </tr>`
+  })
+
+  return `<html>
+  <head>
+    <title>GROQ Specification Versions</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      body {
+        color: #333333;
+        font: 13pt/18pt Cambria, 'Palatino Linotype', Palatino, 'Liberation Serif', serif;
+        margin: 6rem auto 3rem;
+        max-width: 780px;
+      }
+      @media (min-width: 1240px) {
+        body {
+          padding-right: 300px;
+        }
+      }
+      a {
+        color: #3B5998;
+        text-decoration: none;
+      }
+      a:hover {
+        text-decoration: underline;
+      }
+      h1 {
+        font-size: 1.5em;
+        margin: 8rem 0 2em;
+      }
+      td {
+        padding-bottom: 5px;
+      }
+      td + td {
+        padding-left: 2ch;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>GROQ</h1>
+    <table>
+      <tbody>${rows.join('')}</tbody>
+    </table>
+  </body>
+  </html>`
+}
+
+async function main() {
+  const versions = [{
+    slug: "draft",
+    name: "Working Draft",
+    date: new Date(),
+    variant: "Prerelease"
+  }]
+
+  const lines = fs.readFileSync(0).toString().trim().split("\n")
+  let isFirst = true;
+
+  for (const line of lines) {
+    const [version, date] = line.split(" ")
+    versions.push({
+      slug: version,
+      name: version,
+      date: Number(date) * 1000,
+      variant: isFirst ? "Latest release" : "",
+    })
+    isFirst = false
+  }
+
+  console.log(render(versions))
+}
+
+main()
---
+++ scripts/publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# Inspired by:
+# https://github.com/graphql/graphql-spec/blob/main/publish.sh
+
+PUBLISH_DIR="gh-pages"
+
+echo "Building spec"
+npm run build > /dev/null 2>&1
+
+mkdir -p "$PUBLISH_DIR"
+
+# Replace /draft with this build.
+echo "Publishing to: /draft"
+rm -rf "$PUBLISH_DIR/draft"
+cp -R out/ "$PUBLISH_DIR/draft"
+
+CURRENT_VERSION=$(git tag --points-at HEAD | grep 'v\d.*')
+
+# If this is a tagged commit, publish to a permalink and index.
+if [ -n "$CURRENT_VERSION" ]; then
+  echo "Publishing to: /$CURRENT_VERSION"
+  cp -R out/ "$PUBLISH_DIR/$CURRENT_VERSION"
+fi
+
+# Update index
+echo "Updating index"
+git tag -l --format='%(refname:lstrip=2) %(creatordate:unix)' | sort -r |
+  node scripts/generate-index.js > "$PUBLISH_DIR/index.html"
+
+echo "Done"
