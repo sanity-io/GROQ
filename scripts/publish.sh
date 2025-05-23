43c6250fd332eff8777c25659be685f8e5fc1055
Tonina <zhelyazkova.tonina@gmail.com>
2025-05-23 10:50:29 +0200

    chore: more logging

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,8 +17,14 @@ cp -R out/ "$PUBLISH_DIR/draft"
 
 echo "Current head: $(git rev-parse HEAD)"
 echo "Current dir: $(pwd)"
+echo "Current branch: $(git branch --show-current)"
+echo "Current tag: $(git tag --points-at HEAD)"
+echo "Current SHA: $GITHUB_SHA"
+echo "All tags: $(git tag -l)"
 
 CURRENT_VERSION=$(git tag --points-at $GITHUB_SHA | grep 'GROQ-\d.*')
+
+echo "Current version: $CURRENT_VERSION"
 
 # If this is a tagged commit, publish to a permalink and index.
 if [ -n "$CURRENT_VERSION" ]; then
